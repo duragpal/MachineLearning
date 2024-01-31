2dacf168b6da42fc05518e0ed5f84bb79f3408ba
Durag Pal Singh <107455742+duragpal@users.noreply.github.com>
2024-01-31 20:11:11 +0530

    deleted

--- 01-Python/abcedarian series.py
+++
@@ -1,6 +0,0 @@
-str1="ABCDEF"
-str2="ate"
-str3=''.join(sorted(str1))
-strnew=""
-for i in str3:
-    print(i+str2,end=" ")
--- 01-Python/downrighttriangle.py
+++
@@ -1,8 +0,0 @@
-n=5
-for i in range(0,n):
-    for j in range(0,i):
-        print(" ",end="")
-    for j in range(0,5-i):
-        print("*",end="")
-    print()    
-    
